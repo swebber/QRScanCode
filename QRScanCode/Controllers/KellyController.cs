f30721c87669d9e67bf364d39381cb233b6f4ce6
R. Scott Webber <scott@notss.com>
2014-02-23 14:14:18 -0500

    Commit solution

---
+++ QRScanCode/Controllers/KellyController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QRScanCode.Controllers
+{
+    public class KellyController : Controller
+    {
+        //
+        // GET: /Kelly/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Test()
+        {
+            return View();
+        }
+    }
+}
---
+++ QRScanCode/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QRScanCode.MvcApplication" Language="C#" %>
