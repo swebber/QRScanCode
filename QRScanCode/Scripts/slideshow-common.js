a71a38554e8f321e7dd2876b669f2ece4279dc2b
R. Scott Webber <scott@notss.com>
2014-02-23 17:11:14 -0500

    Code cleanup.

---
+++ QRScanCode/Scripts/slideshow-common.js
@@ -0,0 +1,33 @@
+﻿jQuery(document).ready(function ($) {
+
+    var options = {
+        $FillMode: 1,                                       //[Optional] The way to fill image in slide, 0 stretch, 1 contain (keep aspect ratio and put all inside slide), 2 cover (keep aspect ratio and cover whole slide), 4 actuall size, default value is 0
+        $DragOrientation: 1,                                //[Optional] Orientation to drag slide, 0 no drag, 1 horizental, 2 vertical, 3 either, default value is 1 (Note that the $DragOrientation should be the same as $PlayOrientation when $DisplayPieces is greater than 1, or parking position is not 0)
+        $SlideDuration: 900,                                //[Optional] Specifies default duration (swipe) for slide in milliseconds, default value is 500
+        $DirectionNavigatorOptions: {
+            $Class: $JssorDirectionNavigator$,              //[Requried] Class to create direction navigator instance
+            $ChanceToShow: 2,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
+            $AutoCenter: 2,                                 //[Optional] Auto center arrows in parent container, 0 No, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
+            $Steps: 1                                       //[Optional] Steps to go for each navigation request, default value is 1
+        }
+    };
+
+    var jssor_slider1 = new $JssorSlider$("slider1_container", options);
+    //responsive code begin
+    //you can remove responsive code if you don't want the slider scales while window resizes
+    function ScaleSlider() {
+        var parentWidth = jssor_slider1.$Elmt.parentNode.clientWidth;
+        if (parentWidth)
+            jssor_slider1.$SetScaleWidth(parentWidth);
+        else
+            window.setTimeout(ScaleSlider, 30);
+    }
+
+    //Scale slider immediately
+    ScaleSlider();
+
+    if (!navigator.userAgent.match(/(iPhone|iPod|iPad|BlackBerry|IEMobile)/)) {
+        $(window).bind('resize', ScaleSlider);
+    }
+    //responsive code end
+});
